439d5974f1d1b73816a2527f0ef9363ddb14ddb8
Hazurl <ricoujules69@gmail.com>
2019-10-01 00:54:19 +0200

    Fix color printing

--- src/sftk/print/Printer.cpp
+++ src/sftk/print/Printer.cpp
@@ -7,7 +7,7 @@ std::ostream& operator<<(std::ostream& os, sf::Time const& t) {
 }
 
 std::ostream& operator<<(std::ostream& os, sf::Color const& c) {
-    return os << "Color {" << c.r << ", " << c.g << ", " << c.b << "}";
+    return os << "Color {" << static_cast<unsigned>(c.r) << ", " << static_cast<unsigned>(c.g) << ", " << static_cast<unsigned>(c.b) << "}";
 }
 
 std::ostream& operator<<(std::ostream& os, sf::ContextSettings const& c) { 
